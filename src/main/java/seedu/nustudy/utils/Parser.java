bf2e6ce46df3322ad2690ddb8c64cee2bbf90a02
Foo Kang <fookang3@gmail.com>
2025-10-09 17:45:51 +0800

    Add parser to parse user input and return a Command
    - Split input into command and arguments
    - Map 'add' to AddCourseCommand
    - Throw NUStudyCommandException for unknow commands

---
+++ src/main/java/seedu/nustudy/utils/Parser.java
@@ -0,0 +1,26 @@
+package seedu.nustudy.utils;
+
+import seedu.nustudy.command.AddCourseCommand;
+import seedu.nustudy.command.Command;
+import seedu.nustudy.exceptions.NUStudyCommandException;
+
+public class Parser {
+    /**
+     * Return a Command parsed from user's input
+     *
+     * @param input The user-inputted command string
+     * @return A Command object that can execute the user's request
+     * @throws NUStudyCommandException If the command is invalid
+     */
+    public static Command parseCommand(String input)
+            throws NUStudyCommandException {
+        String[] words = input.split("\\s+", 2);
+        String command = words[0].toLowerCase();
+        String arguments = words.length > 1 ? words[1] : "";
+
+        return switch (command) {
+        case "add" -> new AddCourseCommand(arguments);
+        default -> throw new NUStudyCommandException("Wrong command");
+        };
+    }
+}
