5f6db23e234a87bc3ace26c102cd5e343db5b663
Foo Kang <fookang3@gmail.com>
2025-10-09 17:42:22 +0800

    Add console UI
    - printLineBreak() for visual separator
    - readCommand() to read trimmed user input
    - printCourseAdded() to confirm added course

---
+++ src/main/java/seedu/nustudy/ui/UserInterface.java
@@ -0,0 +1,35 @@
+package seedu.nustudy.ui;
+
+import seedu.nustudy.course.Course;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    private static final String LINE_BREAK = "____________________________________________________________";
+    private static final Scanner INPUT_SCANNER = new Scanner(System.in);
+
+    /**
+     * Print a partition line for visual separation
+     */
+    public static void printLineBreak() {
+        System.out.println(LINE_BREAK);
+    }
+
+    /**
+     * Returns the user's command.
+     *
+     * @return The trimmed user input command
+     */
+    public String readCommand() {
+        return INPUT_SCANNER.nextLine().trim();
+    }
+
+    /**
+     * Print a success message when a course is added
+     *
+     * @param course The course that was added
+     */
+    public static void printCourseAdded(Course course) {
+        System.out.println(course + " added");
+    }
+}
