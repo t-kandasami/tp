e2fafddf076a8559e120a2e96a13e5fedc3cdc3d
Andrew <e1398281@u.nus.edu>
2025-10-10 03:14:14 +0800

    Add ConfirmationHandler utility with parsing

--- src/main/java/seedu/nustudy/utils/Parser.java
+++ src/main/java/seedu/nustudy/utils/Parser.java
@@ -2,11 +2,12 @@ package seedu.nustudy.utils;
 
 import seedu.nustudy.command.AddCourseCommand;
 import seedu.nustudy.command.Command;
+import seedu.nustudy.command.ResetCourseHoursCommand;
 import seedu.nustudy.exceptions.NUStudyCommandException;
 
 public class Parser {
     /**
-     * Return a Command parsed from user's input
+     * Returns a Command parsed from user's input
      *
      * @param input The user-inputted command string
      * @return A Command object that can execute the user's request
@@ -18,9 +19,13 @@ public class Parser {
         String command = words[0].toLowerCase();
         String arguments = words.length > 1 ? words[1] : "";
 
-        return switch (command) {
-        case "add" -> new AddCourseCommand(arguments);
-        default -> throw new NUStudyCommandException("Wrong command");
-        };
+        switch (command) {
+		case "add":
+			return new AddCourseCommand(arguments);
+		case "reset":
+			return new ResetCourseHoursCommand(arguments);
+		default:
+			throw new NUStudyCommandException("Wrong command");
+		}
     }
 }
