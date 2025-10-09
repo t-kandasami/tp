9067a5f03d2fa4f9b477e6c26994b7c05588ff60
Andrew <e1398281@u.nus.edu>
2025-10-09 23:46:12 +0800

    Merge branch 'master' of https://github.com/AY2526S1-CS2113-W14-2/tp

---
+++ src/main/java/seedu/nustudy/exceptions/NUStudyException.java
@@ -0,0 +1,13 @@
+package seedu.nustudy.exceptions;
+
+public class NUStudyException extends Exception {
+
+    /**
+     * Create a new NUStudyException with the specific error message
+     *
+     * @param message The error message describing the error
+     */
+    public NUStudyException(String message) {
+        super(message);
+    }
+}
