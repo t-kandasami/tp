f47414f469f19ee73518ed8172071144c8a6d365
Foo Kang <fookang3@gmail.com>
2025-10-09 17:35:18 +0800

    Add Course and CourseManager classes
    - Add Course class to represent a course
    - Add CourseManager for storing and adding courses

---
+++ src/main/java/seedu/nustudy/course/CourseManager.java
@@ -0,0 +1,19 @@
+package seedu.nustudy.course;
+
+import java.util.ArrayList;
+
+/**
+ * A list to manage all  courses in the application
+ */
+public class CourseManager {
+    private final ArrayList<Course> courses = new ArrayList<>();
+
+    /**
+     * Add a course to the list
+     *
+     * @param course The course to add
+     */
+    public void add(Course course) {
+        courses.add(course);
+    }
+}
