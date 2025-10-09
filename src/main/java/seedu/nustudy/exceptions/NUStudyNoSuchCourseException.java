7cb783bad2ff0e2d1dccaf60f772eeec2ef501c2
Andrew <e1398281@u.nus.edu>
2025-10-10 03:15:01 +0800

    Add ResetCourseHoursCommand command with exception

---
+++ src/main/java/seedu/nustudy/exceptions/NUStudyNoSuchCourseException.java
@@ -0,0 +1,7 @@
+package seedu.nustudy.exceptions;
+
+public class NUStudyNoSuchCourseException extends NUStudyException {
+	public NUStudyNoSuchCourseException(String message) {
+		super(message);
+	}
+}
