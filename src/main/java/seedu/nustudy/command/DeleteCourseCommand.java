75c26fe89801de0935ed5eaf5febbf1974c326fb
T. Kandasami <t.kandasami1@gmail.com>
2025-10-10 14:48:59 +0800

    Feature: Add DeleteCourseCommand
    * The NUStudy Application now supports deletion of course using coursename
    * This feature has error handling and is integrated to the application

---
+++ src/main/java/seedu/nustudy/command/DeleteCourseCommand.java
@@ -0,0 +1,34 @@
+package seedu.nustudy.command;
+
+import seedu.nustudy.course.Course;
+import seedu.nustudy.course.CourseManager;
+import seedu.nustudy.exceptions.NUStudyException;
+import seedu.nustudy.ui.UserInterface;
+
+public class DeleteCourseCommand implements Command {
+    private final String input;
+
+    public DeleteCourseCommand(String input) {
+        this.input = input;
+    }
+
+
+    public void execute(CourseManager courses) throws NUStudyException {
+        if(input.isEmpty()) {
+            throw new NUStudyException("Please enter a course name that you want to delete");
+        }
+        Course courseToDelete = courses.findCourse(input);
+        courses.delete(courseToDelete);
+        UserInterface.printCourseDeleted(courseToDelete);
+    }
+
+    /**
+     * Indicate whether this command should exit the application
+     *
+     * @return false, as adding course does not exit the application
+     */
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
