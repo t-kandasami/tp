d72d3ce3f21a9de2323c8bb3cc20fc1f1c38dc30
Andrew <e1398281@u.nus.edu>
2025-10-18 00:40:48 +0800

    test: add JUnit tests for loading functionalities

--- src/test/java/arpa/home/nustudy/course/CourseTest.java
+++ src/test/java/arpa/home/nustudy/course/CourseTest.java
@@ -3,11 +3,20 @@ package arpa.home.nustudy.course;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
+import arpa.home.nustudy.exceptions.NUStudyException;
+
 class CourseTest {
 
     @Test
     void testStorageStringConversion() {
         Course course = new Course("CS2113");
         assertEquals("C|CS2113", course.toStorageString());
+    }
+
+    @Test
+    void testStringStorageConversion() throws NUStudyException {
+        String storedCourseName = "C|CS2113";
+        Course course = Course.fromStorageString(storedCourseName);
+        assertEquals("CS2113", course.getCourseName());
     }
 }
--- src/test/java/arpa/home/nustudy/session/SessionTest.java
+++ src/test/java/arpa/home/nustudy/session/SessionTest.java
@@ -1,15 +1,26 @@
 package arpa.home.nustudy.session;
 
 import arpa.home.nustudy.course.Course;
+import arpa.home.nustudy.exceptions.NUStudyException;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class SessionTest {
     @Test
-    void testStorageStringConversion() {
+    void testStringStorageConversion() {
         Course course = new Course("CS2113");
         Session session = new Session(course, 5);
         assertEquals("S|CS2113|5", session.toStorageString());
+    }
+
+    @Test
+    void testStorageStringConversion() throws NUStudyException {
+        String storedCourseName = "S|CS2113|5";
+        Object[] expected = {"CS2113", 5};
+        Object[] actual = Session.fromStorageString(storedCourseName);
+        assertArrayEquals(expected, actual);
     }
 }
