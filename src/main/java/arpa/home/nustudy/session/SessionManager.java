f64bcc7a9c15d93be60319758c82162ad33f38a7
Foo Kang <fookang3@gmail.com>
2025-10-25 01:31:23 +0800

    feat: add getAllDateStringsForCourse(Course)

--- src/main/java/arpa/home/nustudy/session/SessionManager.java
+++ src/main/java/arpa/home/nustudy/session/SessionManager.java
@@ -1,5 +1,6 @@
 package arpa.home.nustudy.session;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -14,8 +15,8 @@ public class SessionManager implements Iterable<Session> {
      * @param course      The course object want to add study session
      * @param loggedHours The amount of hours spent for a study session
      */
-    public void add(final Course course, final int loggedHours) {
-        sessions.add(new Session(course, loggedHours));
+    public void add(final Course course, final int loggedHours, final LocalDate date) {
+        sessions.add(new Session(course, loggedHours, date));
     }
 
     public boolean sessionExists (final Course course, final int loggedHours) {
@@ -29,7 +30,9 @@ public class SessionManager implements Iterable<Session> {
 
     /**
      * Get a list of all logged hours for a specific course
+     *
      * @param course The course to get all logged hours of
+     *
      * @return List of all logged hours
      */
     public ArrayList<Integer> getAllLoggedHoursForCourse(final Course course) {
@@ -43,6 +46,18 @@ public class SessionManager implements Iterable<Session> {
             res.add(session.getLoggedHours());
         }
 
+        return res;
+    }
+
+    public ArrayList<String> getAllDateStringsForCourse(final Course course) {
+        final ArrayList<String> res = new ArrayList<>();
+
+        for (final Session session : sessions) {
+            if (!session.getCourse().equals(course)) {
+                continue;
+            }
+            res.add(session.getDateString());
+        }
         return res;
     }
 
