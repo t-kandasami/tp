871b06b21b533df994ad1a941261af9ca1576411
T. Kandasami <t.kandasami1@gmail.com>
2025-10-26 13:54:40 +0800

    enhancement: add confirmation message for deleteCourseCommand

--- src/main/java/arpa/home/nustudy/command/DeleteCourseCommand.java
+++ src/main/java/arpa/home/nustudy/command/DeleteCourseCommand.java
@@ -9,17 +9,39 @@ import arpa.home.nustudy.exceptions.NUStudyException;
 import arpa.home.nustudy.exceptions.NUStudyNoSuchCourseException;
 import arpa.home.nustudy.session.SessionManager;
 import arpa.home.nustudy.ui.UserInterface;
+import arpa.home.nustudy.utils.ConfirmationHandler;
 import arpa.home.nustudy.utils.Storage;
+
+//@@author t-kandasami
+
+/**
+ * Handles the deletion of a course from the CourseManager. Prompts user for confirmation before deleting the specified
+ * course. Provides feedback through the UserInterface and logs actions.
+ */
 
 public class DeleteCourseCommand implements Command {
     private static final Logger logger = Logger.getLogger(Storage.class.getName());
     private final String input;
 
+    /**
+     * Constructs a DeleteCourseCommand with the specified course name.
+     *
+     * @param input Name of the course to be deleted.
+     */
     public DeleteCourseCommand(final String input) {
         this.input = input;
         logger.log(Level.FINE, "DeleteCourseCommand created with input: {0}", input);
     }
 
+    /**
+     * Executes the deletion of the course from the CourseManager after confirmation. Throws NUStudyException if the
+     * course cannot be deleted or does not exist.
+     *
+     * @param courses  The CourseManager containing all courses.
+     * @param sessions The SessionManager used for session-related actions (not used here).
+     *
+     * @throws NUStudyException If the course cannot be deleted or is not found.
+     */
     @Override
     public void execute(final CourseManager courses, final SessionManager sessions) throws NUStudyException {
         logger.log(Level.INFO, "Executing DeleteCourseCommand with input: {0}", input);
@@ -28,7 +50,7 @@ public class DeleteCourseCommand implements Command {
 
         if (input.isEmpty()) {
             logger.log(Level.WARNING, "DeleteCourseCommand failed: Input Course Name is empty");
-            throw new NUStudyException("Please enter a course name that you want to delete");
+            throw new NUStudyException("Please enter a course name to delete");
         }
 
         logger.log(Level.FINE, "DeleteCourseCommand starts a search for course: {0}", input);
@@ -38,14 +60,24 @@ public class DeleteCourseCommand implements Command {
             logger.log(Level.WARNING, "Course not found: {0}", input);
             throw new NUStudyNoSuchCourseException("Course does not exist");
         }
+
+        UserInterface.printCourseDeletedConfirmation(courseToDelete);
+        final boolean confirmation =
+                ConfirmationHandler.firstLevelConfirmation(
+                        "Are you sure you want to delete " + courseToDelete);
+
+        if (!confirmation) {
+            throw new NUStudyException(courseToDelete + " has not been deleted as per your request");
+        }
         assert (courseToDelete.getCourseName().equals(input)) : "Returned course name does "
                 + "match input";
+
         courses.delete(courseToDelete);
 
         if (courses.findCourse(input) == null) {
-            logger.log(Level.WARNING, "Successfully deleted course: {0}", courseToDelete.getCourseName());
+            logger.log(Level.INFO, "Successfully deleted course: {0}", courseToDelete);
         } else {
-            logger.log(Level.SEVERE, "Course deletion failed: {0}", courseToDelete.getCourseName());
+            logger.log(Level.SEVERE, "Course deletion failed: {0}", courseToDelete);
             throw new NUStudyException("Course deletion failed");
         }
 
@@ -64,3 +96,4 @@ public class DeleteCourseCommand implements Command {
         return false;
     }
 }
+//@@author
