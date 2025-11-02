547f102edde267ab4da43085681a87f0e7cf7094
T. Kandasami <t.kandasami1@gmail.com>
2025-11-03 01:50:04 +0800

    refactor: Change HourValidator to throw NUStudyException

--- src/main/java/arpa/home/nustudy/utils/HourValidator.java
+++ src/main/java/arpa/home/nustudy/utils/HourValidator.java
@@ -1,24 +1,24 @@
 package arpa.home.nustudy.utils;
 
-import arpa.home.nustudy.exceptions.NUStudyException;
+import arpa.home.nustudy.exceptions.NUStudyCommandException;
 
 public class HourValidator {
-    private static final int MAX_INT = 24;
-    private static final int MIN_INT = 1;
+    private static final int MAX = 24;
+    private static final double MIN = 0.5;
 
     private static boolean isValidHours(final double hours) {
-        return hours >= MIN_INT && hours <= MAX_INT && (hours * 2) % 1 == 0;
+        return hours >= MIN && hours <= MAX && (hours * 2) % 1 == 0;
     }
 
-    public static double parseHours(final String input) throws NUStudyException {
+    public static double parseHours(final String input) throws NUStudyCommandException {
         double hours;
         try {
             hours = Double.parseDouble(input);
         } catch (NumberFormatException e) {
-            throw new NUStudyException("Hours must be a double");
+            throw new NUStudyCommandException("Hours must be a double");
         }
         if (!isValidHours(hours)) {
-            throw new NUStudyException("Hours must be between 1 and 24, in 0.5 increments.");
+            throw new NUStudyCommandException("Hours must be between 0.5 and 24, in 0.5 increments.");
         }
         return hours;
     }
