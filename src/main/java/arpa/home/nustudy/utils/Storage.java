96537df001ec53adc7c15c13ec9d7243170785fe
Andrew <e1398281@u.nus.edu>
2025-11-03 15:07:16 +0800

    fix: Remove unused import from 'Storage' class

--- src/main/java/arpa/home/nustudy/utils/Storage.java
+++ src/main/java/arpa/home/nustudy/utils/Storage.java
@@ -2,7 +2,6 @@ package arpa.home.nustudy.utils;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
